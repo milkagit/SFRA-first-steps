fa3d0f7c975371289db037d7d3f9b15b51def5a1
Milka Deneva <milka.deneva@raketadesign.com>
2021-11-10 00:02:45 +0200

    Add homework

---
+++ cartridges/app_storefront_base/cartridge/controllers/Default.js
@@ -0,0 +1,62 @@
+"use strict";
+
+/**
+ * @namespace Default
+ */
+
+var server = require("server");
+var cache = require("*/cartridge/scripts/middleware/cache");
+var consentTracking = require("*/cartridge/scripts/middleware/consentTracking");
+var pageMetaData = require("*/cartridge/scripts/middleware/pageMetaData");
+
+var userLoggedIn = require("*/cartridge/scripts/middleware/userLoggedIn");
+
+/** when sitepath is defined in the site aliases from business manager, homepage will be rendered directly */
+/**
+ * Default-Start : This end point is the root of the site, when opening from the BM this is the end point executed
+ * @name Base/Default-Start
+ * @function
+ * @memberof Default
+ * @param {middleware} - consentTracking.consent
+ * @param {middleware} - cache.applyDefaultCache
+ * @param {category} - non-sensitive
+ * @param {renders} - isml
+ * @param {serverfunction} - get
+ */
+server.get(
+    "Start",
+    consentTracking.consent,
+    cache.applyDefaultCache,
+    server.middleware.https,
+    userLoggedIn.validateLoggedIn,
+    consentTracking.consent,
+    function (req, res, next) {
+        var Site = require("dw/system/Site");
+        var PageMgr = require("dw/experience/PageMgr");
+        var pageMetaHelper = require("*/cartridge/scripts/helpers/pageMetaHelper");
+
+        var CustomerMgr = require("dw/customer/CustomerMgr");
+        var Resource = require("dw/web/Resource");
+        var URLUtils = require("dw/web/URLUtils");
+
+        pageMetaHelper.setPageMetaTags(req.pageMetaData, Site.current);
+
+        var page = PageMgr.getPage("homepage");
+
+        if (page && page.isVisible()) {
+            res.page("homepage");
+        } else {
+            res.render("home/homePage");
+        }
+        next();
+    },
+    pageMetaData.computedPageMetaData
+);
+
+/** Renders the maintenance page when a site has been set to "Maintenance mode" */
+server.get("Offline", cache.applyDefaultCache, function (req, res, next) {
+    res.render("siteOffline");
+    next();
+});
+
+module.exports = server.exports();
