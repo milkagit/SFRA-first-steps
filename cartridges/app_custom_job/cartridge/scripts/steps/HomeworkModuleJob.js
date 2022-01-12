84148a436c61de6d59e1fc4e6de43fd140645ba5
Milka Deneva <milka.deneva@raketadesign.com>
2022-01-12 23:43:38 +0200

    editing job

--- cartridges/app_custom_job/cartridge/scripts/steps/HomeworkModuleJob.js
+++ cartridges/app_custom_job/cartridge/scripts/steps/HomeworkModuleJob.js
@@ -1,6 +1,36 @@
 var CustomObjectMgr = require("dw/object/CustomObjectMgr");
 var File = require("dw/io/File");
+var FileWriter = require("dw/io/FileWriter");
+var CSVStreamWriter = require("dw/io/CSVStreamWriter");
 
 module.exports.execute = function () {
-  var object = CustomObjectMgr("HomeworkJob");
+  var sfCustomObject = CustomObjectMgr.getAllCustomObjects("HomeworkJob");
+
+  var fileWriter;
+  var csv;
+
+  try {
+    var file = new File([File.IMPEX, "homework.csv"].join(File.SEPARATOR));
+    fileWriter = new FileWriter(file);
+
+    csv = new CSVStreamWriter(fileWriter);
+    // csv.writeStartElement("products");
+    // csv.writeStartDocument();
+
+    while (sfCustomObject.hasNext()) {
+      // will iterate as many time as the custom objects we have
+      var sfObject = sfCustomObject.next();
+      csv.writeNext(sfObject.firstName);
+      csv.writeNext(sfObject.lastName);
+      csv.writeNext(sfObject.email);
+
+      Transaction.wrap(function () {
+        CustomObjetMgr.remove(sfObject);
+      });
+    }
+  } catch (e) {
+  } finally {
+    csv.close();
+    fileWriter.close();
+  }
 };
