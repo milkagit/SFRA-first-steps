dfcc4ca84a4e00f19fe9a2024667a7448780788e
Milka Deneva <milka.deneva@raketadesign.com>
2022-01-12 17:12:14 +0200

    updates

--- cartridges/app_custom_job/cartridge/controllers/subscriptionData.js
+++ cartridges/app_custom_job/cartridge/controllers/subscriptionData.js
@@ -3,6 +3,16 @@
 var server = require("server");
 //Use the following for CSRF protection: add middleware in routes and hidden field on form
 var csrfProtection = require("*/cartridge/scripts/middleware/csrf");
+var consentTracking = require("*/cartridge/scripts/middleware/consentTracking");
+
+/**
+ * @name SubscriptionData-Show
+ * @param {middleware} - consentTracking.consent
+ * @param {middleware} - csrfProtection.generateToken
+ * @param {middleware} - server.middleware.https
+ * @param {renders} - isml
+ * @param {serverfunction} = get
+ */
 
 server.get(
   "Show",
@@ -13,7 +23,7 @@ server.get(
     var subscriptionDataForm = server.forms.getForm("subscriptionData");
     subscriptionDataForm.clear();
 
-    res.render("/subscriptionData/subscriptionData", {
+    res.render("subscriptionData/subscriptionData", {
       actionUrl: actionUrl,
       subscriptionDataForm: subscriptionDataForm,
     });
@@ -30,23 +40,26 @@ server.post(
     var subscriptionDataForm = server.forms.getForm("subscriptionData");
     var URLUtils = require("dw/web/URLUtils");
     var CustomObjectMgr = require("dw/object/CustomObjectMgr");
-
-    // Perform any server-side validation before this point, and invalidate form accordingly.
-
     var Transaction = require("dw/system/Transaction");
+    var UUIDUtils = require("dw/util/UUIDUtils");
+
+    //form validation
+    // if (subscriptionDataForm.valid) {
+    // }
 
     Transaction.wrap(function () {
       var CustomObject = CustomObjectMgr.createCustomObject(
         "HomeworkJob",
-        subscriptionData.email.value
+        subscriptionDataForm.email.value
       );
-      CustomObject.custom.firstName = subscriptionData.firstName.value;
-      CustomObject.custom.lastName = subscriptionData.firstName.value;
-      CustomObject.custom.email = subscriptionData.email.value;
-      CustomObject.custom.gender = subscriptionData.gender.value;
+      //remove the group name storedFormDetails as you don't need groups in this case
+      CustomObject.custom.firstName = subscriptionDataForm.firstName.value;
+      CustomObject.custom.lastName = subscriptionDataForm.lastName.value;
+      CustomObject.custom.email = subscriptionDataForm.email.value;
+      // CustomObject.custom.gender = subscriptionDataForm.gender.value;
 
       res.json({
-        // success: true,
+        success: true,
         redirectUrl: URLUtils.url(
           "SubscriptionData-Show"
           // "SubscriptionData-SubscriptionData"
