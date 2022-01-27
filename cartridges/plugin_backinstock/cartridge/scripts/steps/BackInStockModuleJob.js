f314c8a1582ed2ed9ff2c8eca1af6aa2cad0bda0
Milka Deneva <milka.deneva@raketadesign.com>
2022-01-27 12:37:55 +0200

    removing comments

--- cartridges/plugin_backinstock/cartridge/scripts/steps/BackInStockModuleJob.js
+++ cartridges/plugin_backinstock/cartridge/scripts/steps/BackInStockModuleJob.js
@@ -1,8 +1,7 @@
 var CustomObjectMgr = require("dw/object/CustomObjectMgr");
 var ProductMgr = require("dw/catalog/ProductMgr");
-var Transaction = require('dw/system/Transaction');
-var serviceTwilio = require('../serviceTwilio')
-
+var Transaction = require("dw/system/Transaction");
+var serviceTwilio = require("../serviceTwilio");
 
 module.exports.execute = function () {
   var objectIterator = CustomObjectMgr.getAllCustomObjects(
@@ -24,21 +23,24 @@ module.exports.execute = function () {
       var phoneNumbersArr = phoneNumbers.split(",");
 
       //to do: add ! for testing
-      if(isProductInStock){
-        for each (phone in phoneNumbersArr){
+      if (isProductInStock) {
+        for (let phone of phoneNumbersArr) {
           var sendToNumber = phone;
-          var fromNumber = '+15076462030'
+          var fromNumber = "+15076462030";
 
-          let response = serviceTwilio.getServiceTwilio(fromNumber, sendToNumber)
-          return response
+          let response = serviceTwilio.getServiceTwilio(
+            fromNumber,
+            sendToNumber
+          );
+          return response;
         }
-          Transaction.wrap(function () {
-            CustomObjectMgr.remove(productObject);
-          });
+        Transaction.wrap(function () {
+          CustomObjectMgr.remove(productObject);
+        });
       }
     }
   } catch (err) {
-    throw new Error('Validation error', {cause: err})
+    throw new Error("Validation error", { cause: err });
   } finally {
     objectIterator.close();
   }
